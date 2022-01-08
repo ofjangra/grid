f2846d3fd7ad9ba84e435a9ab9f0a4707162527f
Nahush <zannrez@gmail.com>
2022-01-08 10:50:50 +0530

    modified everything

--- index.js
+++ index.js
@@ -2,97 +2,97 @@ const imgArray = [
 
     {
        
-        imgSrc : "./img/perfume1.jpg",
+        imgSrc : "./img2/perfume1.jpg",
         alt : "perfume",
         title: "Aqua Di Gio POUR HOMME Perfume",
        
     },
     {
       
-        imgSrc : "./img/perfume2.jpg",
+        imgSrc : "./img2/perfume2.jpg",
         alt : "perfume",
         title: "Versace EROS perfume",
       
     },
     {
     
-        imgSrc : "./img/glass1.jpg",
+        imgSrc : "./img2/glass1.jpg",
         alt : "Glass",
         title: "Glasses",
      
     },
     {
         
-        imgSrc : "./img/bracelet.jpg",
+        imgSrc : "./img2/bracelet.jpg",
         alt : "Bracelet",
         title: "Brcelet",
    
     },
     {
  
-        imgSrc : "./img/shoes1.jpg",
+        imgSrc : "./img2/shoes1.jpg",
         alt : "Shoes",
         title: "Brown Sneakers",
      
     },
     {
         
-        imgSrc : "./img/knife.jpg",
+        imgSrc : "./img2/knife.jpg",
         alt : "knife",
         title: "Combat Knife",
   
     },
     {
   
-        imgSrc : "./img/glass2.jpg",
+        imgSrc : "./img2/glass2.jpg",
         alt : "glasses",
         title: "glasses",
     
     },
     {
        
-        imgSrc : "./img/shoes3.jpg",
+        imgSrc : "./img2/shoes3.jpg",
         alt : "Shoes",
         title: "Formal Shoes",
  
     },
     {
-        imgSrc : "./img/shoes2.jpg",
+        imgSrc : "./img2/shoes2.jpg",
         alt : "Shoes",
         title: "Chelsea Boots",
      
     },
     {
    
-        imgSrc : "./img/glass5.jpg",
+        imgSrc : "./img2/glass5.jpg",
         alt : "Glasses",
         title: "Sunglasses",
      
     },
     {
        
-        imgSrc : "./img/watch1.jpg",
+        imgSrc : "./img2/watch1.jpg",
         alt : "Watch",
         title: "Analog Watch",
     
     },
     {
      
-        imgSrc : "./img/perfume3.jpg",
+        imgSrc : "./img2/perfume3.jpg",
         alt : "perfume",
         title: "Diesel Fuel For Life Perfume",
     
     },
     {
      
-        imgSrc : "./img/bracelet1.jpg",
+        imgSrc : "./img2/bracelet1.jpg",
         alt : "Bracelet",
         title: "Bracelet",
         
     },
     {
      
-        imgSrc : "./img/watch2.jpg",
+        imgSrc : "./img2/watch2.jpg",
         alt : "Watch",
         title: "Analog Watch",
        
@@ -101,6 +101,8 @@ const imgArray = [
 
 
 const gridContainer = document.querySelector(".image_gridContainer");
+const modal = document.querySelector('.modal');
+
 
 for(let i=0; i<=imgArray.length-1; i++){
     const imageCard = document.createElement('div');
@@ -117,6 +119,13 @@ for(let i=0; i<=imgArray.length-1; i++){
 
 
 
+    function addImage(e){
+        const modal_image = document.querySelector('.modal_image');
+        const imageTitle = document.getElementById('imageTitle');
+        modal_image.innerHTML = `<img src= ${imgArray[e].imgSrc} alt = ${imgArray[e].alt}/>`
+        imageTitle.innerHTML = imgArray[e].title
+       
+    }
     
 
 
@@ -124,21 +133,68 @@ for(let i=0; i<=imgArray.length-1; i++){
 
     imageCard.addEventListener('click', () =>{
 
-        const modal = document.querySelector('.modal');
+        
         modal.style.display = "flex";
             addImage(i)
         console.log('hello')
-        const prev = document.getElementById('prev');
+
+
+        let prev = document.querySelector('.prev');
+
+
+        let next = document.querySelector('.next');
+
         
+
         prev.addEventListener('click', () =>{
-            
-            if(i >=0 ){
-                addImage(i--)
+            i -= 1;
+
+            if(i>=0){
+                addImage(i)
+                next.style.display ="flex"
+           
+            }
+            if(i<0){
+                prev.style.display = "none";
             }
         })
+
+    
+    
+        
        
+      
+
+
+
+        next.addEventListener('click', () =>{
+            i += 1;
+
+            if(i<=imgArray.length-1){
+                addImage(i)
+                prev.style.display ="flex"
+            }
+            if(i>imgArray.length-1){
+                next.style.display ="none"
+            }
+        })
+
+
     })
+
+    const closeModalBtn = document.querySelector('.closeModal');
+    closeModalBtn.addEventListener('click', () =>{
+        modal.style.display = "none";
+    })
+   
+
+
+
+
+
+  
 }
+
 
 
 
@@ -148,13 +204,6 @@ for(let i=0; i<=imgArray.length-1; i++){
 
     
 
-    function addImage(e){
-        const modal_image = document.querySelector('.modal_image');
-        const imageTitle = document.getElementById('imageTitle');
-        console.log( imgArray)
-        modal_image.innerHTML = `<img src= ${imgArray[e].imgSrc} alt = ${imgArray[e].alt}/>`
-        imageTitle.innerHTML = imgArray[e].title
-       
-    }
+  
 
   
